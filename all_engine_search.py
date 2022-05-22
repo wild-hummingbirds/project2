82ed2519ca2ca91a1eb20abead24e8af287d1962
sateshr <ramnathsatesh100@gmail.com>
2022-05-21 21:53:39 -0400

    Modify all engine search based on new duckduckgo, etc.

--- all_engine_search.py
+++ all_engine_search.py
@@ -1,11 +1,16 @@
-from duckduckgo_sel import duckduckgo_nav
-from google_sel import google_nav
+from duck_search_api import duckduckgo_search
 from bing_search_api import bing_search_api
+from google_sel import google_nav
 from dotenv import load_dotenv
 load_dotenv()
 
 def all_engines_data(query):
     all_data = []
+
+    print('Searching DuckDuckGo.....')
+    duck_data = duckduckgo_search(query)    
+    print('Total DuckDuckGo results: ', len(duck_data))
+    print('\n\n')
 
     
     print('Searching Bing.....')
@@ -13,19 +18,16 @@ def all_engines_data(query):
     print('Total Bing results: ', len(bing_data))
     print('\n')
 
+
     print('Searching Google.....')
     google_data = google_nav(query)
     print('Total Google results: ', len(google_data))
     print('\n')
 
-    print('Searching DuckDuckGo.....')
-    duck_data = duckduckgo_nav(query)    
-    print('Total DuckDuckGo results: ', len(duck_data))
-    print('\n\n')
-
+    all_data.extend(duck_data)
     all_data.extend(bing_data)
     all_data.extend(google_data)
-    all_data.extend(duck_data)
+    
 
     return all_data
 
