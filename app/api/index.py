61f018a9dc7aeb2748a5735319fce21f9cf91a55
Tyron Samaroo <tyronsamaroo828@gmail.com>
2022-05-23 18:16:35 -0400

    Updated based on type

--- app/api/index.py
+++ app/api/index.py
@@ -3,7 +3,7 @@ from app.api.search_form import SearchForm
 from app.api.all_engine_search import all_engines_data
 from app.helpers.main import data_dump
 
-from app.helpers.sql_helper import get_data, check_search_query_exist
+from app.helpers.sql_helper import get_data, check_search_query_exist, get_data_pdf, get_data_web
 
 index_bp = Blueprint('index', __name__, template_folder='templates')
 
@@ -39,6 +39,8 @@ def search():
     form = SearchForm()
     q = form.searchbox.data.lower()
     num_res = int(form.result.data)
+    type_res = form.type_res.data
+    print(type_res)
     print(num_res)
     if form.validate_on_submit():
         q_exist = check_search_query_exist(q)
@@ -62,12 +64,52 @@ def search():
     # else:
     #     pass
 
+@index_bp.route('/all', methods=['GET', 'POST'])
+def search_all():
+    all = [1, 2, 3, 4]
+    res = "You searched for nothing"
+    form = SearchForm()
+    q = form.searchbox.data.lower()
+    content_type = form.type_res.data
+    num_res = int(form.result.data)
+    print(num_res)
+    if form.validate_on_submit():
+        q_exist = check_search_query_exist(q)
+        if q_exist:
+            if content_type == "ALL":
+                res = get_data(q, num_res)
+            if content_type == "PDF":
+                res = get_data_pdf(q, num_res)
+            if content_type == "WEB":
+                res = get_data_web(q, num_res)
+            return render_template('search.html', data=res, form=form, query=q, valid_form=form.validate_on_submit())
+        
+        else:
+            print("Searching....")
+            data_dump(q, num_res)
+            if content_type == "ALL":
+                res = get_data(q, num_res)
+            if content_type == "PDF":
+                res = get_data_pdf(q, num_res)
+            if content_type == "WEB":
+                res = get_data_web(q, num_res)
+            return render_template('search.html', data=res, form=form, query=q, valid_form=form.validate_on_submit())
+    flash('No Search Query Provided', 'error')
+    return render_template('search.html', form=form, query=q, valid_form=form.validate_on_submit())
+
+
+# @index_bp.route('/custom')
+# def search():
+#     return render_template('search.html')
 
 
 
 @index_bp.route('/about')
 def about():
     return render_template('about.html')
+
+
+
 
 
 
--- app/api/search_form.py
+++ app/api/search_form.py
@@ -6,5 +6,5 @@ from wtforms.validators import DataRequired
 class SearchForm(FlaskForm):
     searchbox = StringField('', validators=[DataRequired()])
     result = SelectField('Results', choices=[(5, 5), (10, 10),(20, 20),(30, 30)])
-    redo = BooleanField('Redo Search')
+    type_res = SelectField('Type', choices=[("ALL", "ALL"), ("PDF", "PDF"),("Web", "Web")])
     submit = SubmitField('Search')
