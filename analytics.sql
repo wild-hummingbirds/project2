9562841d6b5951c7e8ac60dbdc3e5260eab36a24
Ivan Miller <ivan.miller@voxmedia.com>
2022-05-23 20:05:17 -0400

    updated analytics sql

--- analytics.sql
+++ analytics.sql
@@ -1,4 +1,5 @@
--- 1. bringing all of the relevant information together
+
+ -- 1. bringing all of the relevant information together
 SELECT DISTINCT
  q.search_query,
 -- u.engine_name, 
@@ -29,14 +30,18 @@ LEFT JOIN WildHummingbirds.search_query q ON u.search_id = q.search_id
 WHERE freq IS NOT NULL
 ORDER BY search_query, total_freq DESC;
 
+
+WITH base as (
 -- 2.2 search engine ranking
 SELECT 
 search_query,
 engine_name,
-total_freq 
+total_freq,
+search_engine_rank 
 FROM (
 	SELECT b.*, 
-	COUNT(engine_name) OVER(PARTITION BY search_query) AS search_engine_count
+	COUNT(engine_name) OVER(PARTITION BY search_query) AS search_engine_count,
+    DENSE_RANK() OVER(PARTITION BY search_query ORDER BY total_freq DESC) as search_engine_rank
 	FROM (
 		SELECT DISTINCT
 		search_query,
@@ -59,4 +64,20 @@ FROM (
 	)b
 )c 
 WHERE search_engine_count >= 3
-ORDER BY search_query, total_freq DESC;
+ORDER BY search_query, total_freq DESC
+)
+ SELECT 
+ engine_name,
+ -- ones as top_count,
+ ROUND(ones / SUM(ones) OVER(),2)*100.0 as freq
+ FROM
+(
+SELECT 
+engine_name,
+SUM(CASE WHEN search_engine_rank=1 THEN 1 ELSE NULL END) as ones
+-- SUM(CASE WHEN search_engine_rank=2 THEN 1 ELSE NULL END) as twos,
+-- SUM(CASE WHEN search_engine_rank=3 THEN 1 ELSE NULL END) as threes
+FROM base
+GROUP BY 1
+ORDER BY 1 DESC
+) a
